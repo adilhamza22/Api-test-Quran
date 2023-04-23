3a267ff22e137262651ec9204a1c3f82d30c8b6c
Muhammad Hamza Adil <105517506+adilhamza22@users.noreply.github.com>
2023-04-23 23:52:48 +0500

    Add files via upload
    2 passed 1 failed

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testMatch: ['**/tests/**/*.test.js'], // specify the test file pattern
+    testEnvironment: 'node', // set the test environment
+    reporters: ['default'], // specify the reporter(s) to use
+  };
+  
